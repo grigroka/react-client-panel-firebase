f618c6aed6482248857ee3a7cd22348d4f2aa8f5
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-13 15:59:03 +0200

    navbar implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import AppNavbar from './components/layout/AppNavbar';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AppNavbar />
         <h1>Hello World!</h1>
       </div>
     );
