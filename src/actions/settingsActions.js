feee30564a8a8a04b91b5f2852c76632f9cc6cb7
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-17 13:42:41 +0200

    DRY optimization

--- src/actions/settingsActions.js
+++ src/actions/settingsActions.js
@@ -4,50 +4,33 @@ import {
   ALLOW_REGISTRATION
 } from './types';
 
-export const setDisableBalanceOnAdd = () => {
+const handleStorage = setting => {
   // Get settings from local storage
   const settings = JSON.parse(localStorage.getItem('settings'));
-
   // Toggle
-  settings.disableBalanceOnAdd = !settings.disableBalanceOnAdd;
-
+  settings[setting] = !settings[setting];
   // Set back to local storage
   localStorage.setItem('settings', JSON.stringify(settings));
+  return settings[setting];
+};
 
+export const setDisableBalanceOnAdd = () => {
   return {
     type: DISABLE_BALANCE_ON_ADD,
-    payload: settings.disableBalanceOnAdd
+    payload: handleStorage('disableBalanceOnAdd')
   };
 };
 
 export const setDisableBalanceOnEdit = () => {
-  // Get settings from local storage
-  const settings = JSON.parse(localStorage.getItem('settings'));
-
-  // Toggle
-  settings.disableBalanceOnEdit = !settings.disableBalanceOnEdit;
-
-  // Set back to local storage
-  localStorage.setItem('settings', JSON.stringify(settings));
-
   return {
     type: DISABLE_BALANCE_ON_EDIT,
-    payload: settings.disableBalanceOnEdit
+    payload: handleStorage('disableBalanceOnEdit')
   };
 };
 
 export const setAllowRegistration = () => {
-  // Get settings from local storage
-  const settings = JSON.parse(localStorage.getItem('settings'));
-
-  // Toggle
-  settings.allowRegistration = !settings.allowRegistration;
-
-  // Set back to local storage
-  localStorage.setItem('settings', JSON.stringify(settings));
-
   return {
     type: ALLOW_REGISTRATION,
-    payload: settings.allowRegistration
+    payload: handleStorage('allowRegistration')
   };
 };
