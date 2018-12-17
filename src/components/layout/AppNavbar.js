a95d8d186475008b3c33e32e8cc091657fe9236c
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-17 13:54:39 +0200

    conditional register and login links

--- src/components/layout/AppNavbar.js
+++ src/components/layout/AppNavbar.js
@@ -28,6 +28,7 @@ export class AppNavbar extends Component {
 
   render() {
     const { auth } = this.props;
+    const { allowRegistration } = this.props.settings;
     // const { isAuthenticated } = this.state;
     return (
       <nav className="navbar navbar-expand-md navbar-dark bg-primary mb-4">
@@ -73,6 +74,20 @@ export class AppNavbar extends Component {
                   >
                     Logout
                   </a>
+                </li>
+              </ul>
+            ) : null}
+            {allowRegistration && !auth.uid ? (
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link to="/login" className="nav-link">
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/register" className="nav-link">
+                    Register
+                  </Link>
                 </li>
               </ul>
             ) : null}
